b4799c04ec407146fa36086584585af3381319b3
Remika <remika.mito@gmail.com>
2022-08-22 14:20:57 +1000

    Files added to git repository
    In this commit, we add all files in our R package (spidey) to the
    git repository. This includes all functions and their documentation
    along with the Description.

---
+++ R/Zscore_function.R
@@ -0,0 +1,145 @@
+# Z-score function
+# There are 3 main functions here: COMBATTRACTS, RANKINT, COMPUTEZ, and RUNTRACTZ
+# These will perform ComBat, perform rank-based INT, compute Z-scores for tracts for a patient
+
+#--------------------------------------------------------
+
+#' COMBAT function
+#'
+#' This function allows you to perform neuroCombat on tract data, if data are
+#' extracted from multiple sites and/or scanners and require harmonisation.
+#' @param input_data A dataframe containing unharmonised tract data.
+#' @param ID_column The column index corresponding to participant ID
+#' @param tractrange The column(s) corresponding to tract data 
+#' @param scanner_column The column corresponding to site or scanner ID 
+#' @param age_column The column corresponding to participant age
+#' @keywords neuroCombat tracts
+#' @export
+#' @examples
+#' COMBATTRACTS()
+
+COMBATTRACTS <- function(input_data, ID_column, tractrange, scanner_column, age_column) {
+  require(neuroCombat)
+  require(reshape)
+  require(tidyr)
+  require(dplyr)
+  
+  # format dataframe
+  FDCdat <- input_data[, c(ID_column,tractrange)]
+  batch <- input_data[[scanner_column]]
+  tractnames <- colnames(input_data)[tractrange]
+  ID_name <- colnames(FDCdat)[1]
+  TractDat <- FDCdat %>% gather("Tract","value", 2:ncol(FDCdat)) %>% spread(ID_name,value) # turn FDCdat to long
+  dat_final <- TractDat[-c(1)]
+  
+  # set up model including age **NOTE: include flexibility here?
+  age <- input_data[[age_column]]
+  mod <- model.matrix(~age)
+  
+  # run data harmonization
+  combat.harmonized <- neuroCombat(dat=dat_final, batch=batch, mod=mod)
+  
+  # Output combat data
+  harmon_dat <- combat.harmonized$dat.combat # note: this is a matrix 
+  harmon_wide <- as.data.frame(cbind(Tract=tractnames, harmon_dat)) # use as.data.frame to convert matrix to df
+  combat_data <- harmon_wide %>% gather(ID_name,"value", 2:ncol(harmon_wide)) %>% spread(Tract,value)
+  
+  return(combat_data)
+}
+
+#' RANKINT function
+#'
+#' This function performs rank-based inverse normal transformation (INT) on tract
+#' data using the RNOmni 'RankNorm' function. 
+#' @param input_data A dataframe containing tract data.
+#' @param tract Column name of tract data to be normalised
+#' @param int_tract Column name of inverse-normal transformed tract data
+#' @export
+#' @examples
+#' RANKINT()
+
+RANKINT <- function(input_data, tract, int_tract) {
+  require(RNOmni)
+  input_data[[int_tract]] <- RNOmni::RankNorm(input_data[[tract]], k=3/8) #look at syntax for this
+  return(input_data)
+}
+
+#' COMPUTEZ function
+#'
+#' This function computes the mean and SD on tract data across all healthy control 
+#' participants included in the data. Z-scores are then computed for a specified 
+#' patient, compared to the control cohort. 
+#' @param input_data A dataframe containing tract data
+#' @param tract Column name of raw tract data
+#' @param patient_ID The column index corresponding to patient ID
+#' @param HC Healthy control identifier. Defaults to 0
+#' @param ID_column The column index corresponding to participant IDs. Defaults to 1
+#' @param Group_column The column index corresponding to the Group identifier. Defaults to 12
+#' @export
+#' @examples
+#' COMPUTEZ()
+
+COMPUTEZ <- function(input_data, tract, patient_ID, HC = 0, ID_column=1, Group_column=12){
+  tract_INT <- paste(tract, "_INT", sep="")
+  HC_ID <- input_data[Group_column]==HC
+  HCdat <- input_data[HC_ID,]
+  tract_mean <- mean(HCdat[[tract_INT]])
+  tract_SD <- sd(HCdat[[tract_INT]])
+  pt_ID <- input_data[ID_column]==patient_ID
+  pt_dat <- input_data[pt_ID,]
+  tract_Z <- (pt_dat[[tract_INT]] - tract_mean) / tract_SD
+  return(tract_Z)
+}
+
+
+#' RUNTRACTZ function
+#'
+#' This function performs the computation of tract-specific Z-scores for a given patient, 
+#' using the RANKINT and COMPUTEZ functions. A dataframe should be provided as input, 
+#' with a column corresponding to patient ID, and column(s) corresponding to tract-specific 
+#' measures included as a minimum. If data harmonisation is required, columns corresponding 
+#' to site (numeric identifier) and age should also be included
+#' @param input_data A dataframe containing tract data
+#' @param tractrange The column(s) corresponding to tract data 
+#' @param patient_ID The column index corresponding to patient ID
+#' @param combat Perform neuroCombat site/scanner harmonisation? Defaults to TRUE
+#' @param ID_column The column index corresponding to participant IDs. Defaults to 1
+#' @param age_column The column index corresponding to age. Defaults to 8
+#' @export
+#' @examples
+#' RUNTRACTZ()
+
+
+RUNTRACTZ <- function(input_data, tractrange, patient_ID, combat=TRUE, 
+                      ID_column=1, scanner_column=10, age_column=8){
+  # if combat = "TRUE", then run COMBAT, else nah
+  Zdat <- data.frame(ID=patient_ID)
+  # also grab age, ICV, scanner, other important variables into this dataframe?
+  tractnames <- colnames(input_data[,tractrange])
+  if(combat==TRUE){
+    # first combat
+    combat_data <- COMBATTRACTS(input_data, ID_column, tractrange, scanner_column, age_column)
+    combat_data <- cbind(combat_data, Group=input_data$Group) # note: Group column is hardcoded***
+    # then Z-scores
+    for (t in tractnames) {
+      tractname_INT <- paste(t, "_INT", sep="")
+      INT_data <- RANKINT(combat_data, t, tractname_INT) 
+      newdat <- COMPUTEZ(INT_data, t, patient_ID) 
+      Zdat[ , ncol(Zdat) + 1] <- newdat
+      colnames(Zdat)[ncol(Zdat)] <- paste0(t, "_Z")
+    }
+  } else if(combat==FALSE){
+    for (t in tractnames) {
+      tractname_INT <- paste(t, "_INT", sep="")
+      INT_data <- RANKINT(input_data, t, tractname_INT) 
+      newdat <- COMPUTEZ(INT_data, t, patient_ID) 
+      Zdat[ , ncol(Zdat) + 1] <- newdat
+      colnames(Zdat)[ncol(Zdat)] <- paste0(t, "_Z")
+    }
+  }
+  
+  return(Zdat)
+}
+
+
+
---
+++ R/spider_function.R
@@ -0,0 +1,105 @@
+# Function to plot Spideys
+# Date: 9 August 2022
+
+#-----------------------------------------------------------------------------------
+
+#' SPIDERZ function
+#'
+#' This function plots tract-based data as a radar (or spider) chart. Input data 
+#' should be tract-specific Z-scores for a given patient, when compared to a 
+#' control cohort. Z-scores can be computed using the RUNTRACTZ function if input 
+#' data contains tract data for both healthy control participants and the patient.
+#' @param input_Zscores A dataframe containing Z-scores for tract data for a patient.
+#' @param bilateral I have not done this yet. Defaults to TRUE.
+#' @param sigline Include a significance threshold line (Z=-1.96) in plot? Defaults to TRUE 
+#' @param customtracts I have not done this yet. Defaults to FALSE 
+#' @param leftcol Colour for left-sided tracts. Defaults to "#3498DB"
+#' @param rightcol Colour for left-sided tracts. Defaults to "#E74C3C"
+#' @export
+#' @examples
+#' SPIDERZ()
+
+SPIDERZ <- function(input_Zscores, bilateral=TRUE, sigline=TRUE, customtracts=FALSE,
+                    leftcol="#3498DB", rightcol="#E74C3C"){
+  require(fmsb)
+  require(scales)
+  
+  pt_ID <- input_Zscores[,"ID"] # note this is hardcoded, should maybe include an identifier
+  
+  # first set up left and right data **note: should probably grep these directly from data
+  roi_names <- c("AF", "CG", "CST", "FX", "IFOF", "ILF", "SCP", "SLF III", "SLF II", "SLF I", "UF")
+  
+  leftnames <- grep("left",colnames(input_Zscores), value=TRUE)
+  rightnames <- grep("right",colnames(input_Zscores), value=TRUE)
+  
+  left_data <- input_Zscores[c(leftnames)]
+  colnames(left_data) <- roi_names
+  rownames(left_data) <- c("Left")
+  
+  right_data <- input_Zscores[c(rightnames)]
+  colnames(right_data) <- roi_names
+  rownames(right_data) <- c("Right")
+  
+  # set up rows of zeros, maxes and sig
+  zeros <- data.frame(matrix(0, nrow = 1, ncol = length(roi_names)))
+  colnames(zeros) <- roi_names
+  
+  maxes <- data.frame(matrix(-4, nrow = 1, ncol = length(roi_names)))
+  colnames(maxes) <- roi_names
+  
+  sig <- data.frame(matrix(-1.96, nrow = 1, ncol = length(roi_names)))
+  colnames(sig) <- roi_names
+  
+  # assign tract order
+  col_order <- c("SLF III", "SLF II", "SLF I",
+                 "ILF", "IFOF", "UF", "AF",
+                 "FX", "CG",
+                 "SCP", "CST")
+  
+  # bind data and add significance line 
+  if(sigline==TRUE){
+    radardatmm <- rbind(zeros, maxes, sig, left_data, right_data) 
+    radardatmm <- radardatmm[, col_order]
+    hemi_color <- c("black", leftcol, rightcol) 
+    
+    hemi_lty <- c(2,1,1)
+    hemi_lwd <- c(1,4,4)
+    
+    colors_border <- hemi_color
+    colors_in <- c(alpha(hemi_color[1],0),alpha(hemi_color[2],0.3),alpha(hemi_color[3],0.3))
+    
+  } else if(sigline==FALSE){
+    radardatmm <- rbind(zeros, maxes, left_data, right_data) 
+    radardatmm <- radardatmm[, col_order]
+    hemi_color <- c(leftcol, rightcol) 
+    
+    hemi_lty <- c(1,1)
+    hemi_lwd <- c(4,4)
+    
+    colors_border <- hemi_color
+    colors_in <- c(alpha(hemi_color[1],0.3),alpha(hemi_color[2],0.3))
+    
+  }
+  
+  # grid specifications ***NOTE: include a +1 as well??
+  axislab <- c(-4,-3,-2,-1,"HC mean")
+  
+  grid_lty <- c(1,2,2,2,2)
+  grid_cols <- c("black", "grey","grey","grey","grey")
+  
+  radarplot <- fmsb::radarchart( radardatmm  , axistype=4 , #seg = 6,
+                                 #custom polygon
+                                 pcol=colors_border, pfcol=colors_in, plwd=hemi_lwd , plty=hemi_lty, 
+                                 #custom the grid
+                                 cglcol="grey", cglty=1, axislabcol="grey", cglwd=0.8, # cglty=c(2,1,3,5,6,2,2,2,2), 
+                                 #custom labels
+                                 vlcex=0.8, vlabels=col_order,
+                                 #custom axis
+                                 caxislabels=axislab,
+                                 #title
+                                 title=pt_ID
+  ) 
+  
+  return(radarplot)
+}
+
