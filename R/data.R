c0e8d869a52b6e4dbbe9aad6d622b11e1a11ad2d
Remika <remika.mito@gmail.com>
2022-09-01 11:56:00 +1000

    Patient tract data finalised and updated.

--- R/data.R
+++ R/data.R
@@ -1,15 +1,15 @@
 #' Tract FDC data from fixel-based analysis of DWI data
 #'
-#' Data from a cohort of healthy control participants and 
-#' patients with epilepsy. The data included are mean fibre density 
+#' Data from a cohort of healthy control participants and
+#' patients with epilepsy. The data included are mean fibre density
 #' and cross-section (FDC) values from selected tracts of interest,
-#' and were computed using the fixel-based analysis pipeline. 
+#' and were computed using the fixel-based analysis pipeline.
 #'
 #' @docType data
 #'
 #' @usage data(tractdata)
 #'
-#' @format A data frame with 102 rows and 36 variables:
+#' @format A data frame with 100 rows and 36 variables:
 #' \describe{
 #'   \item{Group}{Group of participant, either patient (1) or control (0)}
 #'   \item{Scanner_ID}{identifier of scanner or site, for use in ComBat}
